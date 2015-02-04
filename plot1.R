b08b9bc959e1d345a3343ec4170a70baa2d9f114
yrldj <yrldj@qq.com>
2015-02-04 16:03:51 +0800

    Delete plot1.R

--- plot1.R
+++
@@ -1,8 +0,0 @@
-dat<-read.table("./household_power_consumption.txt",header=FALSE,
-                skip=66637,nrows=2880,sep=";",col.names=c("Date","Time","Global_active_power",
-                "Global_reactive_power","Voltage","Global_intensity","Sub_metering_1",
-                "Sub_metering_2","Sub_metering_3"),colClasses=c("character","character","numeric",
-                "numeric","numeric","numeric","numeric","numeric","numeric"))
-png(file="plot1.png",width = 480, height = 480)
-hist(dat[,3],col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-dev.off()
